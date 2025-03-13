44ef960d1d08b1154e63e853bcca52f393c43ce6
DESKTOP-O5TQC3N\Walek <bwalek@gmail.com>
2025-03-13 16:48:21 +0100

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,79 @@
-import React from 'react';
-import './App.css'
+import React, {useState, useEffect} from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MovieListHeading from './components/MovieListHeading';
 import MovieList from './components/MovieList';
+import SearchBox from './components/SearchBox';
+import AddFavourite from './components/AddFavourite';
+import RemoveFavourite from './components/RemoveFavourite';
 
 function App() {
 
-  const movies = [
-    {
-      "Title": "Starwars: Goretech",
-      "Year": "2018",
-      "imdbID": "tt9336300",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOGUyMjY5YjMtZTYzNS00ZjIyLTk5MDMtZmMzNGI0MWZmZDVkXkEyXkFqcGc@._V1_SX300.jpg"
-  },
-  {
-      "Title": "MeUndies x StarWars: The Dark Side",
-      "Year": "2018",
-      "imdbID": "tt9414858",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMzg3ZDcwNDQtOTVlMC00ZjE0LThiYjktY2U2YTVjODU5MWY5XkEyXkFqcGdeQXVyNjg3MDM0MzE@._V1_SX300.jpg"
-  },
-  {
-      "Title": "MeUndies x StarWars: The Force",
-      "Year": "2018",
-      "imdbID": "tt9414918",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BZTdjYjM1YzgtYmViMS00ODFjLTgyM2EtMjRkNTFiNDEyMTU2XkEyXkFqcGdeQXVyNjg3MDM0MzE@._V1_SX300.jpg"
-  },
-  {
-      "Title": "40 Years: StarWars & ILM",
-      "Year": "2016",
-      "imdbID": "tt5320598",
-      "Type": "movie",
-      "Poster": "N/A"
+  const [searchValue, setSearchValue] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=aeb2b829`;
+    const movieResponse = await fetch(url);
+    const movieResponseJson = await movieResponse.json();
+
+    if(movieResponseJson.Search) {
+      setMovies(movieResponseJson.Search);
+    }
   }
-  ]
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  //stringify - konverze JS objektu na string
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("movie-app-favourites", JSON.stringify(items));
+  }
+
+  const AddFavouriteMovie = (movie) => {
+    if (favourites.includes(movie)) {
+      alert("Tento film už je v Oblíbených");
+    } else {
+      const newFavouriteList = [...favourites, movie];
+      setFavourites(newFavouriteList);
+      saveToLocalStorage(newFavouriteList);
+    }
+  }
+
+  const RemoveFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  }
 
   return (
     <div className="container overflow-hidden">
       <div className="row mt-4 mb-4">
         <MovieListHeading heading="Seznam filmů"></MovieListHeading>
+        <SearchBox searchValue={searchValue}
+        setSearchValue={setSearchValue} />
       </div>
       <div className="row row-cols-3">
-        <MovieList movies={movies}></MovieList>
+        <MovieList 
+        movies={movies}
+        handleFavouritesClick={AddFavouriteMovie}
+        favouriteComponent={AddFavourite}
+        ></MovieList>
+      </div>
+
+      <div className="row mt-4 mb-4">
+        <MovieListHeading heading="Oblíbené filmy"></MovieListHeading>
+      </div>
+      <div className="row row-cols-3">
+        <MovieList 
+        movies={favourites}
+        handleFavouritesClick={RemoveFavouriteMovie}
+        favouriteComponent={RemoveFavourite}
+        ></MovieList>
       </div>
     </div>
   )
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,7 @@
+import noPhotoImage from '../assets/no-image.jpg';
+
 const MovieList = (props) => {
+    const FavouriteComponent = props.favouriteComponent;
     return (
         <>
         {props.movies.map( (movie, index) => ( 
@@ -8,9 +11,13 @@ const MovieList = (props) => {
                         <h4>{movie.Title}</h4>
                     </div>
                     <img className="img-fluid text-center" 
-                    src={movie.Poster} alt="movie" />
-                    <div className="overlay d-flex align-items-center
-                    justify-content-center">    
+                    src={movie.Poster == "N/A" ? noPhotoImage : movie.Poster}
+                    alt="movie" />
+                    <div 
+                    onClick = {() => props.handleFavouritesClick(movie)}
+                    className="overlay d-flex align-items-center
+                    justify-content-center"> 
+                        <FavouriteComponent />   
                     </div>
                 </div>
             </div>
